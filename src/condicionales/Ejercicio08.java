fa00a2402b1228769ce70ee42292c38e4416a03c
soyne <soyne@MSI-NESTOR>
2023-11-05 17:04:22 +0100

    Ejercicio 08 Condicionales completo

--- src/condicionales/Ejercicio08.java
+++ src/condicionales/Ejercicio08.java
@@ -2,18 +2,30 @@ package condicionales;
 
 import java.util.Scanner;
 
+/*
+ * Ejemplo:
+ * 
+ * distancia: 700, dias: 9 | Esperaba: 1750.0€ | Resultado: 1750.0€
+ * 
+ * distancia: 900, dias: 9 | Esperaba: 1575.0€ | Resultado: 1575.0€
+ * 
+ */
+
 public class Ejercicio08 {
 
 	public static void main(String[] args) {
-		
+
+		// PRECIOKM: precio por kilometro
+		final float PRECIOKM = 2.5f;
+
 		// distancia: distancia a recorrer introducida por el usuario
 		float distancia;
-		
+
 		// dias: duracion de la estancia introducidos por el usuario
 		int dias;
-		
-		// precio: precio total del billete de tren
-		int precio;
+
+		// precioTotal: precio total del billete de tren
+		float precioTotal;
 
 		// Inicia el Scanner
 		Scanner sc = new Scanner(System.in);
@@ -23,12 +35,20 @@ public class Ejercicio08 {
 		distancia = sc.nextInt();
 		System.out.println("¿De cuantos dias sera la estancia?");
 		dias = sc.nextInt();
-		
-		// Multiplica la distancia por 2,5 (o yo que se)
-		// If: 
-		if (dias>7 && distancia>800) {
-			
+
+		// Multiplica la distancia por PRECIOKM (o yo que se)
+		precioTotal = distancia * PRECIOKM;
+
+		// If: Si la estancia es mayor a 7 dias y los kilometros recorridos son mas de
+		// 800, se hara un 30% de descuento
+		if (dias > 7 && distancia > 800) {
+
+			precioTotal = precioTotal * 0.7f;
+
 		}
+
+		// Muestra el precio total por pantalla
+		System.out.println("Precio total: " + precioTotal);
 
 		// Cierra el Scanner
 		sc.close();
