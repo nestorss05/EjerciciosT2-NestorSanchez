689196630c36c3a425366f6c002c374d7fce58fd
nestorss05 <nestor.sanchez@iesnervion.es>
2023-11-08 10:15:01 +0100

    Ejercicio 08 Tema 2 Bucles NSS (Incompleto)

---
+++ src/bucles/Ejercicio08.java
@@ -0,0 +1,22 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio08 {
+
+	public static void main(String[] args) {
+		
+		// numero: respectivos numeros introducidos por el usuario
+		int numero;
+		
+		// Abre el Scanner
+		Scanner sc = new Scanner (System.in);
+		
+		// Aqui ira el codigo a implementar
+		
+		// Cierra el Scanner
+		sc.close();
+		
+	}
+
+}
